6514f8b7e3a8d866badb2607f4af33aa80987010
jpdslmj <jpds_lmj@outlook.com>
2019-01-03 22:38:29 +0800

    modifier:ljh date:20190103 modified: 1.图片下载功能

--- src/main/java/com/rbc/common/controller/FileController.java
+++ src/main/java/com/rbc/common/controller/FileController.java
@@ -1,6 +1,5 @@
 package com.rbc.common.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.rbc.common.config.AppConfig;
 import com.rbc.common.domain.FileDO;
 import com.rbc.common.service.FileService;
@@ -11,8 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -177,5 +179,39 @@ public class FileController extends BaseController {
 		return R.error();
 	}
 
+	@RequestMapping(value="/download/{id}")
+	public String  downLoad(@PathVariable Long id, HttpServletResponse response){
+		String fileName = appConfig.getUploadPath() + sysFileService.get(id).getUrl().replace("/files/", "");
+		File file = new File(fileName);
+		if(file.exists()){ //判断文件父目录是否存在
+			response.setContentType("application/force-download");
+			response.setHeader("Content-Disposition", "attachment;fileName=" + sysFileService.get(id).getUrl().replace("/files/", ""));
 
+			byte[] buffer = new byte[1024];
+			FileInputStream fis = null; //文件输入流
+			BufferedInputStream bis = null;
+
+			OutputStream os = null; //输出流
+			try {
+				os = response.getOutputStream();
+				fis = new FileInputStream(file);
+				bis = new BufferedInputStream(fis);
+				int i = bis.read(buffer);
+				while(i != -1){
+					os.write(buffer);
+					i = bis.read(buffer);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				bis.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 }
